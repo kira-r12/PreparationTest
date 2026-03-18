55bcbf23d85e17e8a36d301407d69ec07c82a806
Kira <reshetilokira513@gmail.com>
2026-03-18 18:50:32 +0200

    All

--- Materials.cs
+++ Materials.cs
@@ -1,7 +1,28 @@
+using System.Dynamic;
+
 public abstract class Materials
 {
+    
     public string Name{ get; set;}
-    public int Year {get;  set;}
+    public int _year;
+    public int Year
+    {
+        get
+        {
+            return _year;
+        }
+        set
+        {
+            if (value < 1450|| value > 2026)
+            {
+                _year = 2024;
+            }
+            else
+            {
+                _year = value;
+            }
+        }
+    }
     public Materials(string name, int year)
     {
         Name = name;
